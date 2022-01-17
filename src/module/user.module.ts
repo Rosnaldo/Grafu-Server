b3df98b1b4d767396b2af9211e03483f5f84f841
Andrey <andreytsuzuki@gmail.com>
2022-01-17 17:03:32 -0300

    feat(User): controller findOne

--- src/module/user.module.ts
+++ src/module/user.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from 'src/helper/mongodb';
 import { UserFindController } from './user/controller/find.controller';
+import { UserFindOneRepository } from './user/repository/find-one.repository';
 
 @Module({
   imports: [],
   controllers: [UserFindController],
-  providers: [],
+  providers: [PrismaService, UserFindOneRepository],
 })
 export class UserModule {}
--- src/module/user/controller/find.controller.ts
+++ src/module/user/controller/find.controller.ts
@@ -1,16 +1,18 @@
-import { Logger, Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get } from '@nestjs/common';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { UserFindOneRepository } from '../repository/find-one.repository';
 
 @ApiTags('user')
 @Controller('user')
 export class UserFindController {
+  constructor(private readonly findOneRepository: UserFindOneRepository) {}
+
   @Get()
   @ApiResponse({
     status: 200,
     description: 'Find users.',
   })
-  async handle(): Promise<any> {
-    new Logger('Enter in Create');
-    return {};
+  async handle(@Body() email: string): Promise<any> {
+    return this.findOneRepository.execute({ email });
   }
 }
